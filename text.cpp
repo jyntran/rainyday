440bdd7f7c57c4a6923ff287031544b1368c5652
J. Tran <j.yn.tran@gmailcom>
2014-09-27 11:33:05 -0400

    Removed files

--- text.cpp
+++
@@ -1,8 +0,0 @@
-#include "text.h"
-
-Text::Text(int x, int y, std::string s):x(x), y(y), s(s)  {}
-
-void Text::paint(XInfo &xinfo){
-	XDrawString(xinfo.display, xinfo.pixmap, xinfo.gc[1],
-			x, y, s.c_str(), s.length());
-}
